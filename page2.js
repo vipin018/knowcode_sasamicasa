47da34a5fa4246214bd5415fd06608fd381450a6
Tavishaa <tavishaajaiswall@gmail.com>
2025-01-25 00:33:19 +0530

    updated pages

---
+++ page2.js
@@ -0,0 +1,18 @@
+gsap.from(".section",{
+    opacity:0,
+    duration:2,
+    delay:1,  // scale:0,
+  })
+  
+  
+  gsap.from(".b", {
+    opacity: 0,
+    duration: 2,
+    delay: 1.5, // scale:0,
+  });
+  
+  gsap.from(".header", {
+    opacity: 0,
+    duration: 2,
+    delay: 0.5, // scale:0,
+  });
